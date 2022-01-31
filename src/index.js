452c16c8a697dc784dd4975fe17a5bb347390ff0
aapanasov <aapanasov@gmail.com>
2022-01-31 09:00:36 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketConfig) {
+  const brackets = new Map(bracketConfig);
+  const stack = [];
+
+  for (char of str) {
+    char === brackets.get(stack[stack.length - 1])
+      ? stack.pop()
+      : stack.push(char)
+  }
+
+  return stack.length === 0;
 }
